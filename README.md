9bd86ac12ae38b20effe98c52ac376f8c40d0667
slaapyhoofd <slaapyhoofd@protonmail.com>
2024-11-26 23:37:39 +0100

    WattTheRef Bot v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# WattTheRef
-WattTheRef is a Telegram bot that helps chat groups provide lotery style ref links from users 
+# 🌍 WattTheRef Bot 🌍
+
+Greetings, Planeteers! This is your trusty Captain Planet Referral Bot, here to help manage your green energy referral links.
+
+Our mission is to make the world a greener place, one referral at a time. With the power of your referral links, we can help more people join our cause and switch to clean energy. The power is yours!
+
+## 🌿 Bot Commands 🌿
+
+**/refferal [company] [url]**
+
+Use this command to add or update your referral link for a specific green energy company. Remember, the power is yours to provide a valid URL!
+
+Example: `/refferal Tesla https://www.tesla.com/referral/myreferral`
+
+If you only provide the company name, the bot will return a random referral link for that company, courtesy of a fellow Planeteer.
+
+Example: `/refferal Tesla`
+
+**/refferals [company]**
+
+Use this command to list all the referral links for a specific green energy company. This will show you all the Planeteers who have shared their referral links for that company.
+
+Example: `/refferals Tesla`
+
+## 🌎 Go Planet! 🌎
+
+Remember, Planeteers, the power is yours! Let's make the world a greener place together. Go Planet!
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import 'dotenv/config';
+import { Telegraf } from 'telegraf';
+import { JSONFilePreset } from 'lowdb/node';
+
+interface Referral {
+    url: string;
+    username: string;
+}
+
+interface CompanyReferrals {
+    [userId: string]: Referral;
+}
+
+interface Referrals {
+    [company: string]: CompanyReferrals;
+}
+
+interface Database {
+    referrals: Referrals;
+}
+
+const allowedCompanies = ['Tesla', 'Tibber'];
+const defaultData: Database = {
+    referrals: {
+        Tesla: {},
+        Tibber: {},
+    },
+};
+
+const bot = new Telegraf(process.env.BOT_TOKEN as string);
+
+async function start() {
+    // Read or create db.json
+    const db = await JSONFilePreset<Database>('db.json', defaultData);
+
+    bot.command('refferal', async (ctx) => {
+        const [command, company, url] = ctx.message.text.split(' ');
+        const userId = `${ctx.from.id}`;
+        const username = ctx.from.username || ctx.from.first_name;
+
+        console.log(`${username}, ${ctx.message.text}`);
+
+        if (!allowedCompanies.includes(company as string)) {
+            ctx.reply(`By the power of Gaia, I must inform you that the company you've mentioned is not recognized. Please choose from the following: ${allowedCompanies.join(', ')}`);
+            return;
+        }
+
+        if (url) {
+            const urlRegex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+            if (!urlRegex.test(url)) {
+                ctx.reply("Planeteer, your URL doesn't seem to be in the correct format. Remember, the power is yours to provide a valid URL!");
+                return;
+            }
+
+            await db.update(({ referrals }) => {
+                const dbCompany = referrals[company as string] || {};
+                dbCompany[userId] = { url, username };
+                referrals[company as string] = dbCompany;
+
+                ctx.reply(`🌍 Great work, Planeteer! Your referral link for ${company} has been added. The power is yours! 🌍`);
+            });
+        } else {
+            // Return a random referral link.
+            const referrals = db.data.referrals[company as string] || {};
+            const referralIds = Object.keys(referrals);
+            const randomReferralId = referralIds[Math.floor(Math.random() * referralIds.length)] as string;
+            const randomUrl = referrals[randomReferralId]?.url;
+            const randomUsername = referrals[randomReferralId]?.username;
+
+            ctx.replyWithHTML(`🌍 Here is a referral link for ${company} from Planeteer @${randomUsername}. Go Planet! 🌍\n${randomUrl}`, { link_preview_options: { is_disabled: true } });
+        }
+    });
+
+    bot.command('refferals', (ctx) => {
+        const [command, company] = ctx.message.text.split(' ');
+        const refferals = db.data.referrals[company as string];
+        const username = ctx.from.username || ctx.from.first_name;
+        let message = `🌍 Here are the Planeteers' referral links for ${company}. The power is yours! 🌍\n`;
+
+        console.log(`${username}, ${ctx.message.text}`);
+
+        for (const referralId in refferals) {
+            if (refferals[referralId]) {
+                message += `• Planeteer @${refferals[referralId].username}: ${refferals[referralId].url}\n`;
+            }
+        }
+
+        ctx.replyWithHTML(message, { link_preview_options: { is_disabled: true } });
+    });
+}
+
+start()
+    .then(() => {
+        bot.launch();
+        console.log('WattTheRefBot started!');
+    })
+    .catch((e) => console.error(e));
